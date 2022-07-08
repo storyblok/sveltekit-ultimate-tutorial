bcd7c7edeb1e8bce4c71893a3244b48ee7b5029d
Roberto Butti <roberto.butti@gmail.com>
2022-07-08 23:54:59 +0200

    Using key block , for forcing component update
    https://svelte.dev/docs#template-syntax-key

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -2,10 +2,14 @@
 	import { storyblokEditable, StoryblokComponent } from '@storyblok/svelte';
 
 	export let blok;
+
+	$: console.log('BLOK PAGE changed ', blok.body);
 </script>
 
 <div use:storyblokEditable={blok} class="px-4">
-	{#each blok.body as blok}
-		<StoryblokComponent {blok} />
+	{#each blok.body as b}
+		{#key b}
+			<StoryblokComponent blok={b} />
+		{/key}
 	{/each}
 </div>
