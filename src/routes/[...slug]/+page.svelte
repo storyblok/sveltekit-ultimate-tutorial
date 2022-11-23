5d722ba5b7f7801f2e79e3b5eda93feb0dfb0f9c
josefineschaefer <josefine.schaefer@storyblok.com>
2022-11-23 12:12:30 +0100

    fix: add page announcement

--- src/routes/[...slug]/+page.svelte
+++ src/routes/[...slug]/+page.svelte
@@ -3,14 +3,17 @@
 	import { useStoryblokBridge, StoryblokComponent } from '@storyblok/svelte';
 
 	export let data;
-
 	onMount(() => {
 		if (data.story) {
 			useStoryblokBridge(data.story.id, (newStory) => (data.story = newStory));
 		}
 	});
+	
 </script>
 
+<svelte:head>
+  <title>{data.story.name}</title>
+</svelte:head>
 <div>
 	{#if data.story}
 		<StoryblokComponent blok={data.story.content} />
