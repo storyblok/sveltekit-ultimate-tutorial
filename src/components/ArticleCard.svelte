6e91ec5677f58e6b2ee122a7b2c776e154b0e05c
Roberto Butti <roberto.butti@gmail.com>
2022-12-22 00:06:56 +0100

    Create ArticleCard.svelte

---
+++ src/components/ArticleCard.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let article;
+	export let slug;
+</script>
+
+<a href="/{slug}" class="w-full h-full bg-[#f7f6fd] rounded-[5px] text-center overflow-hidden">
+	<img
+		src="{article.image.filename}/m/600x0"
+		alt={article.image.alt}
+		class="w-full h-48 xl:h-72 object-cover pointer-events-none"
+	/>
+	<div class="p-4">
+		<h3 class="text-xl text-[#1d243d] font-bold mb-3">
+			{article.title}
+		</h3>
+		<div class="line-clamp-4">
+			{article.teaser}
+		</div>
+	</div>
+</a>
