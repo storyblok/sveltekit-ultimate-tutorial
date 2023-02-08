956a0cb9ed9ad4eb5e996101822cd35b74ae8c62
Roberto Butti <roberto.butti@gmail.com>
2023-02-08 16:47:34 +0100

    upgrading vite4.0 and basicssl

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import mkcert from 'vite-plugin-mkcert'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-    plugins: [sveltekit(), mkcert()],
-    server: {
-        https: true
-    }
+  plugins: [sveltekit(), basicSsl()],
+  server: {
+    https: true
+  }
 };
 
 export default config;
