5f6b5d3356174b080f51c323f07d1b5d9dcdac75
josefineschaefer <josefine.schaefer@storyblok.com>
2023-04-04 16:12:58 +0200

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,16 @@
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-    alias: {
-      '@storyblok/svelte': './node_modules/@storyblok/svelte',
-    },
-  },
+	kit: {
+		adapter: adapter(),
+		alias: {
+			'@storyblok/svelte': './node_modules/@storyblok/svelte'
+		}
+	},
 
-  preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-  ],
+	preprocess: vitePreprocess()
 };
 
 export default config;
