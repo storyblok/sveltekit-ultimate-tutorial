1e504e5e6044b83a15afbf9f306430b78043ee2c
josefineschaefer <josefine.schaefer@storyblok.com>
2023-04-04 15:37:18 +0200

    fix: add new vite version and preprocess

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,37 +1,37 @@
-import Feature from "../components/Feature.svelte";
-import Grid from "../components/Grid.svelte";
-import Page from "../components/Page.svelte";
-import Teaser from "../components/Teaser.svelte";
-import Hero from "../components/Hero.svelte";
-import Article from "../components/Article.svelte";
-import PopularArticles from "../components/PopularArticles.svelte";
-import AllArticles from "../components/AllArticles.svelte";
-import { apiPlugin, storyblokInit, useStoryblokApi } from "@storyblok/svelte";
+import Feature from '../components/Feature.svelte';
+import Grid from '../components/Grid.svelte';
+import Page from '../components/Page.svelte';
+import Teaser from '../components/Teaser.svelte';
+import Hero from '../components/Hero.svelte';
+import Article from '../components/Article.svelte';
+import PopularArticles from '../components/PopularArticles.svelte';
+import AllArticles from '../components/AllArticles.svelte';
+import { apiPlugin, storyblokInit, useStoryblokApi } from '@storyblok/svelte';
 
 /** @type {import('./$types').LayoutLoad} */
 export async function load() {
-  storyblokInit({
-    accessToken: "MX4ACP2TErhMWLJh8QIFlwtt",
-    use: [apiPlugin],
-    components: {
-      feature: Feature,
-      grid: Grid,
-      page: Page,
-      teaser: Teaser,
-      hero: Hero,
-      article: Article,
-      'popular-articles': PopularArticles,
-      'all-articles': AllArticles
-    },
-  });
-  let storyblokApi = await useStoryblokApi();
-  const dataConfig = await storyblokApi.get('cdn/stories/config/', {
-    version: 'draft',
-    resolve_links: 'url'
-  });
+	storyblokInit({
+		accessToken: 'MX4ACP2TErhMWLJh8QIFlwtt',
+		use: [apiPlugin],
+		components: {
+			feature: Feature,
+			grid: Grid,
+			page: Page,
+			teaser: Teaser,
+			hero: Hero,
+			article: Article,
+			'popular-articles': PopularArticles,
+			'all-articles': AllArticles
+		}
+	});
+	let storyblokApi = await useStoryblokApi();
+	const dataConfig = await storyblokApi.get('cdn/stories/config/', {
+		version: 'draft',
+		resolve_links: 'url'
+	});
 
-  return {
-    storyblokApi: storyblokApi,
-    header: dataConfig.data.story.content.header_menu
-  };
+	return {
+		storyblokApi: storyblokApi,
+		header: dataConfig.data.story.content.header_menu
+	};
 }
