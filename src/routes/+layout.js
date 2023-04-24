0bede89f67650eb2af27979e086df019e6896f03
josefineschaefer <josefine.schaefer@storyblok.com>
2023-04-24 10:04:11 +0200

    chore: add basic SSL for https

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,24 +1,25 @@
-import Feature from "../components/Feature.svelte";
-import Grid from "../components/Grid.svelte";
-import Page from "../components/Page.svelte";
-import Teaser from "../components/Teaser.svelte";
-import { apiPlugin, storyblokInit, useStoryblokApi } from "@storyblok/svelte";
+import Feature from '../components/Feature.svelte';
+import Grid from '../components/Grid.svelte';
+import Page from '../components/Page.svelte';
+import Teaser from '../components/Teaser.svelte';
+import { apiPlugin, storyblokInit, useStoryblokApi } from '@storyblok/svelte';
 
 /** @type {import('./$types').LayoutLoad} */
 export async function load() {
-  storyblokInit({
-    accessToken: "W1vLyxT5rQ15jBpANjnv0gtt",
-    use: [apiPlugin],
-    components: {
-      feature: Feature,
-      grid: Grid,
-      page: Page,
-      teaser: Teaser,
-    },
-  });
-  let storyblokApi = await useStoryblokApi();
+	storyblokInit({
+		accessToken: 'W1vLyxT5rQ15jBpANjnv0gtt',
+		use: [apiPlugin],
+		components: {
+			feature: Feature,
+			grid: Grid,
+			page: Page,
+			teaser: Teaser
+		},
+		https: true
+	});
+	let storyblokApi = await useStoryblokApi();
 
-  return {
-    storyblokApi: storyblokApi,
-  };
+	return {
+		storyblokApi: storyblokApi
+	};
 }
